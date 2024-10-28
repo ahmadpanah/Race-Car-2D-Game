bea6c552bfdefa6402366f52f063694eaa28c1c3
Hossein <djalicrt@gmail.com>
2024-10-28 15:14:49 +0330

    Add Gradient to Fuel Bar

--- Assets/Scripts/FuelController.cs
+++ Assets/Scripts/FuelController.cs
@@ -7,29 +7,37 @@ public class FuelController : MonoBehaviour
     public static FuelController instance;
 
     [SerializeField] private Image _fuelImage;
-    [SerializeField, Range(0.1f,5f)] private float _fuelDrainSpeed = 1f;
+    [SerializeField, Range(0.1f, 5f)] private float _fuelDrainSpeed = 1f;
     [SerializeField] private float _maxFuelAmount = 100f;
+
+    [SerializeField] private Gradient _fuelGradient;
 
     private float _currentFuelAmount;
 
-    private void Awake() {
-        if (instance == null) {
+    private void Awake()
+    {
+        if (instance == null)
+        {
             instance = this;
         }
     }
 
-    private void Start(){
+    private void Start()
+    {
         _currentFuelAmount = _maxFuelAmount;
         UpdateUI();
     }
 
-    private void Update(){
+    private void Update()
+    {
         _currentFuelAmount -= Time.deltaTime * _fuelDrainSpeed;
         UpdateUI();
     }
 
-    private void UpdateUI(){
+    private void UpdateUI()
+    {
         _fuelImage.fillAmount = (_currentFuelAmount / _maxFuelAmount);
+        _fuelImage.color = _fuelGradient.Evaluate(_fuelImage.fillAmount);
     }
- 
+
 }
